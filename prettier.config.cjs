5fc19b6f4d814ebcce4d83983c06b46e0c2a0c3c
Viktor Malmedal <viktor@malmedal.dev>
2023-09-28 09:06:57 +0200

    wip

---
+++ prettier.config.cjs
@@ -0,0 +1,25 @@
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
+/** @type { PrettierConfig | TailwindConfig } */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tailwindConfig: "./tailwind.config.cjs",
+};
+
+module.exports = config;
---
+++ src/store/activePiece.ts
@@ -0,0 +1,3 @@
+import { atom } from "nanostores";
+
+export const activePiece = atom<number | null>(null);
