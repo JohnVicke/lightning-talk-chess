682312f950a4da7426984d24b99f6082055a0395
Viktor Malmedal <viktor@malmedal.dev>
2023-10-04 22:00:00 +0200

    slide config

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { z, defineCollection, reference } from "astro:content";
+ 
+const slideSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  next: reference("slide").optional(),
+  prev: reference("slide").optional()
+});
+ 
+ 
+const slide = defineCollection({
+  type: "content",
+  schema: slideSchema,
+});
+ 
+ 
+export const collections = {
+  slide
+};
+ 
+export type Slide = z.infer<typeof slideSchema>;
