1774ce402a0da1dec7fd1f9e68101ea7a81a7035
Viktor Malmedal <viktor@malmedal.dev>
2023-10-05 22:08:32 +0200

    get session return null

--- src/modules/auth/get-session.ts
+++ src/modules/auth/get-session.ts
@@ -1,15 +1,16 @@
 import type { AstroCookies } from "astro";
+import { HTTP_ONLY_AUTH_COOKIE_NAME } from "config/constants";
 import { Session } from "lib/upstash/session";
 
 export async function getSession(cookies: AstroCookies) {
-  if (!cookies?.has("session")) {
-    return false;
+  if (!cookies?.has(HTTP_ONLY_AUTH_COOKIE_NAME)) {
+    return null;
   }
 
-  const sessionId = cookies.get("session")?.value;
+  const sessionId = cookies.get(HTTP_ONLY_AUTH_COOKIE_NAME)?.value;
 
   if (!sessionId) {
-    return false;
+    return null;
   }
 
   const profile = await Session.get(`session-${sessionId}`);
