a8d0b0a98e4a82501b7a7b5a512908637c3ce61d
Viktor Malmedal <viktor@malmedal.dev>
2023-10-05 22:09:07 +0200

    add locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+import type { Session } from "lib/upstash/session";
+
+declare namespace App {
+  interface Locals {
+    session: Session | null;
+  }
+}
